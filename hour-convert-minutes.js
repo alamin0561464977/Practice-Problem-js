023ad33d3e39409af4cf6f4d0f00e822e0676399
alamin0561464977 <amin23435678901315@gmail.com>
2022-08-05 02:26:44 +0300

    added a hour convert minutes

---
+++ hour-convert-minutes.js
@@ -0,0 +1,9 @@
+/*
+Write a function that will take hour as the input parameter and will
+convert it into minutes and will return the result in minutes.
+*/
+function hourConvertMinutes(hour) {
+    const minutes = hour * 60;
+    return minutes
+};
+console.log(hourConvertMinutes(3))
